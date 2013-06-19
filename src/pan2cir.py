b81bb8193d50bc97b8eee9676e71524d88517d95
Andrey Del Pozo <andrey.delpozo@gmail.com>
2013-06-19 18:27:11 -0500

    4 sample AA

--- src/pan2cir.py
+++ src/pan2cir.py
@@ -1,9 +1,12 @@
 import numpy as np
 import Image
 import math
-
-#Image.fromarray(pix).
-pan = np.array(Image.open('panorama.jpg'))
+import sys
+#from collections import defaultdict #multimap aprox
+inputFile = 'IMG_0134.jpg'
+#'panorama.jpg'
+outputFile = 'result3.jpg'
+pan = np.array(Image.open(inputFile))
 w = pan.shape[1]
 h = pan.shape[0]
 
@@ -28,23 +31,54 @@ def cir2Pan(theta,r,panSize,flip=True):
 		y = r
 	x = panSize[0]*theta/(2*math.pi)
 	return (x,y)
-
+print 'mapping'
 for cc in range(3):
+	print 'Channel:',cc
 	a = pan[:,:,cc]
 	b = cir[:,:,cc]
 	#rows
 	for i in xrange(b.shape[0]):
+		print i,'/',b.shape[0]
+
 		for j in xrange(b.shape[1]):
-			dstCir = car2cir(i-b.shape[0]/2,j-b.shape[1]/2)
-			srcCar = cir2Pan(dstCir[0],dstCir[1],[a.shape[1],a.shape[0]])
-			if srcCar == None:
-				continue
+			dstCirs = []
+			dstCirs.append(car2cir(i-b.shape[0]/2+.5,j-b.shape[1]/2+.5));
+			dstCirs.append(car2cir(i-b.shape[0]/2+.5,j-b.shape[1]/2));
+			dstCirs.append(car2cir(i-b.shape[0]/2,j-b.shape[1]/2+.5));
+			dstCirs.append(car2cir(i-b.shape[0]/2,j-b.shape[1]/2));
+			
+			ps = [];		
+			for dstCir in dstCirs:
+				srcCar = cir2Pan(dstCir[0],dstCir[1],[a.shape[1],a.shape[0]])
+				if srcCar == None:
+					continue
 
-			p = a[srcCar[1],srcCar[0]];
-			b[i,j] = p;
+				ps.append(a[srcCar[1],srcCar[0]]);
+			
+			if len(ps) != 0:
+				b[i,j] = sum(ps)/len(ps)
+			else:
+				b[i,j] = 0
 
+
+print 'saving'
 result = Image.fromarray(np.uint8(cir))
-result.show()
-result.save('result.jpg')
+# result.show()
+result.save(outputFile)
 
 
+#mappings = defaultdict(list)
+#
+#for i in xrange(cir.shape[0]):
+#	for j in xrange(cir.shape[1]):
+# 		dstCir = car2cir(i-cir.shape[0]/2,j-cir.shape[1]/2)
+# 		srcCar = cir2Pan(dstCir[0],dstCir[1],[pan.shape[1],pan.shape[0]])
+# 		if srcCar == None:
+# 			continue
+
+# 		mappings[srcCar].append((i,j))
+
+
+# xx = []
+# for k in mappings:
+# 	xx.append(len(mappings[k])) 
